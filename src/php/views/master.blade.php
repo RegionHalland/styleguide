168be4f7af908890b29439c42e5d4c6e1ccaa888
David Öhlin <hello@davidohlin.se>
2017-10-16 08:56:55 +0200

    added syntax highlighting with prism

--- src/php/views/master.blade.php
+++ src/php/views/master.blade.php
@@ -3,8 +3,148 @@
 <head>
 	<title>Styleguide</title>
 	<link rel="stylesheet" type="text/css" href="../dist/css/main.css">
+	<style type="text/css">
+	/* http://prismjs.com/download.html?themes=prism&languages=markup+css+clike+javascript */
+	/**
+	 * prism.js default theme for JavaScript, CSS and HTML
+	 * Based on dabblet (http://dabblet.com)
+	 * @author Lea Verou
+	 */
+	
+	code[class*="language-"],
+	pre[class*="language-"] {
+		color: black;
+		background: none;
+		text-shadow: 0 1px white;
+		font-family: Consolas, Monaco, 'Andale Mono', 'Ubuntu Mono', monospace;
+		text-align: left;
+		white-space: pre;
+		word-spacing: normal;
+		word-break: normal;
+		word-wrap: normal;
+		line-height: 1.5;
+	
+		-moz-tab-size: 4;
+		-o-tab-size: 4;
+		tab-size: 4;
+	
+		-webkit-hyphens: none;
+		-moz-hyphens: none;
+		-ms-hyphens: none;
+		hyphens: none;
+	}
+	
+	pre[class*="language-"]::-moz-selection, pre[class*="language-"] ::-moz-selection,
+	code[class*="language-"]::-moz-selection, code[class*="language-"] ::-moz-selection {
+		text-shadow: none;
+		background: #b3d4fc;
+	}
+	
+	pre[class*="language-"]::selection, pre[class*="language-"] ::selection,
+	code[class*="language-"]::selection, code[class*="language-"] ::selection {
+		text-shadow: none;
+		background: #b3d4fc;
+	}
+	
+	@media print {
+		code[class*="language-"],
+		pre[class*="language-"] {
+			text-shadow: none;
+		}
+	}
+	
+	/* Code blocks */
+	pre[class*="language-"] {
+		padding: 1em;
+		margin: .5em 0;
+		overflow: auto;
+	}
+	
+	:not(pre) > code[class*="language-"],
+	pre[class*="language-"] {
+		background: #f5f2f0;
+	}
+	
+	/* Inline code */
+	:not(pre) > code[class*="language-"] {
+		padding: .1em;
+		border-radius: .3em;
+		white-space: normal;
+	}
+	
+	.token.comment,
+	.token.prolog,
+	.token.doctype,
+	.token.cdata {
+		color: slategray;
+	}
+	
+	.token.punctuation {
+		color: #999;
+	}
+	
+	.namespace {
+		opacity: .7;
+	}
+	
+	.token.property,
+	.token.tag,
+	.token.boolean,
+	.token.number,
+	.token.constant,
+	.token.symbol,
+	.token.deleted {
+		color: #905;
+	}
+	
+	.token.selector,
+	.token.attr-name,
+	.token.string,
+	.token.char,
+	.token.builtin,
+	.token.inserted {
+		color: #690;
+	}
+	
+	.token.operator,
+	.token.entity,
+	.token.url,
+	.language-css .token.string,
+	.style .token.string {
+		color: #a67f59;
+		background: hsla(0, 0%, 100%, .5);
+	}
+	
+	.token.atrule,
+	.token.attr-value,
+	.token.keyword {
+		color: #07a;
+	}
+	
+	.token.function {
+		color: #DD4A68;
+	}
+	
+	.token.regex,
+	.token.important,
+	.token.variable {
+		color: #e90;
+	}
+	
+	.token.important,
+	.token.bold {
+		font-weight: bold;
+	}
+	.token.italic {
+		font-style: italic;
+	}
+	
+	.token.entity {
+		cursor: help;
+	}		
+	</style>
 </head>
-<body class="pt3">
+<body class="pt3 language-html">
 	<div class="mx-auto max-width-4">
 		<div class="col col-12 sm-col-3 md-col-3 px2">
 			<nav class="mb4">
@@ -71,8 +211,6 @@
 		</div>
 		@yield('content')
 	</div>
-	<!--<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/styles/default.min.css">-->
-	<!--<script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/highlight.min.js"></script>-->
-	<!--<script>hljs.initHighlightingOnLoad();</script>-->
+	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/prism/1.8.1/prism.min.js"></script>
 </body>
 </html>
