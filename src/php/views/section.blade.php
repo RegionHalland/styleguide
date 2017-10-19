363751f390b088b68ccb2755455831e92e188603
David Öhlin <hello@davidohlin.se>
2017-10-19 11:36:13 +0200

    add sizes to icons and modifiers to section view

--- src/php/views/section.blade.php
+++ src/php/views/section.blade.php
@@ -5,10 +5,21 @@
 	@foreach ($section as $key => $value)
 		<h2>{{$value->name}}</h2>
 		{!! $value->description !!}
+		@if (isset($section[0]->state))
+		<section class="py3">
+		    <h3>Modifiers</h3>
+		    <ul>
+		    @foreach ((array)$section[0]->state as $state)
+		        <li>
+		            <code>{{ $state->name }}</code> - {!! $state->description !!}
+		        </li>
+		    @endforeach
+		    </ul>
+		</section>
+		@endif
 		@foreach ($value->markup as $key => $value)
 			<div class="py3">{!! $value->example !!}</div>
 			<pre class="language-html"><code>{{ trim($value->example) }}</code></pre>
-
 		@endforeach
 		<small><strong>Source file:</strong> {{ $value->path }}</small>
 		<hr class="my4">
