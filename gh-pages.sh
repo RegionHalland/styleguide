8a147fc91f8a4b5a591ec8ea48b1086217af1afd
David Öhlin <hello@davidohl.in>
2017-11-06 11:44:11 +0100

    improve vertical nav, add gh-pages script

---
+++ gh-pages.sh
@@ -0,0 +1,11 @@
+# checkout to the gh-pages branch
+git checkout gh-pages
+
+# pull the latest updates
+git rebase master
+
+# push to the origin
+git push origin gh-pages
+
+# checkout to the master branch
+git checkout master
