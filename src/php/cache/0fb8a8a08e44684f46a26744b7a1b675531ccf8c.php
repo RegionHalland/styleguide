99ada1ede074e205f35efcbb4a3e108a16746544
David Öhlin <hello@davidohl.in>
2018-03-12 11:27:39 +0100

    add breadcrumbs component 🍞🔥

--- src/php/cache/0fb8a8a08e44684f46a26744b7a1b675531ccf8c.php
+++
@@ -1,6 +0,0 @@
-<?php $__env->startSection('content'); ?>
-<article>
-	<h1>Home</h1>
-</article>
-<?php $__env->stopSection(); ?>
-<?php echo $__env->make('master', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
