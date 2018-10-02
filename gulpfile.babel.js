e550696705f2b9998ada737dac685124c867e5e3
David Öhlin <hello@davidohl.in>
2018-10-02 14:40:45 +0200

    add built css

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -8,24 +8,23 @@ import tailwindcss from 'tailwindcss'
 gulp.task('css', () => {
 	const plugins = [
 		tailwindcss('tailwind.config.js'),
-		autoprefixer
 	]
 
-	return gulp.src('src/scss/main.css')
+	return gulp.src('src/css/main.css')
 		.pipe(postcss(plugins))
-		.pipe(gulp.dest('build/'))
+		.pipe(gulp.dest('dist/'))
 })
 
 gulp.task('browsersync', () => {
-    browsersync.init({
-    	files: 'build/*.css'
-    })
+	browsersync.init({
+		files: 'build/*.css'
+	})
 })
 
 // Browsersync reload
 gulp.task('bs-reload', () => {
 	browsersync.reload()
-});
+})
 
 // Watch
 gulp.task('watch', ['css', 'browsersync'], () => {
