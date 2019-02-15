8767f88aa37ec7b341644a6f7fa079c28734217a
Viktor Sarge <viktor.sarge@regionhalland.se>
2019-02-15 14:10:44 +0100

    Generated new static files

--- build/js/components.js
+++ build/js/components.js
@@ -1,7 +1,35 @@
 "use strict";
+"use strict";
 
 //look ES6!!
 var onBtnClick = function onBtnClick(element) {
   element.innerHTML = 'You did it! 👍';
   console.log('tadaa');
 };
+"use strict";
+
+var videoPlayButton,
+    videoWrapper = document.getElementsByClassName('rh-video')[0],
+    video = document.getElementsByTagName('video')[0],
+    videoMethods = {
+  renderVideoPlayButton: function renderVideoPlayButton() {
+    if (videoWrapper.contains(video)) {
+      this.formatVideoPlayButton();
+      video.classList.add('has-media-controls-hidden');
+      videoPlayButton = document.getElementsByClassName('video-button')[0];
+      videoPlayButton.addEventListener('click', this.hideVideoPlayButton);
+    }
+  },
+  formatVideoPlayButton: function formatVideoPlayButton() {
+    videoWrapper.insertAdjacentHTML('beforeend', '\
+            <div class="video-button"><div class="video-button-content"> > </div></div>\
+            ');
+  },
+  hideVideoPlayButton: function hideVideoPlayButton() {
+    video.play();
+    videoPlayButton.classList.add('is-hidden');
+    video.classList.remove('has-media-controls-hidden');
+    video.setAttribute('controls', 'controls');
+  }
+};
+videoMethods.renderVideoPlayButton();
--- public/js/components.js
+++ public/js/components.js
@@ -1,11 +1,11 @@
 "use strict";
+"use strict";
 
 //look ES6!!
 var onBtnClick = function onBtnClick(element) {
   element.innerHTML = 'You did it! 👍';
   console.log('tadaa');
 };
-"use strict";
 "use strict";
 
 var videoPlayButton,
