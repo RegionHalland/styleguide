11d635e2de9ebb40caa394c2d1bcf809d8ac0ca8
molvig <jennie@brightfactory.se>
2019-02-27 20:29:45 +0100

    #81, #74, #78, #83, #88

--- public/js/components.js
+++ public/js/components.js
@@ -8,34 +8,6 @@ var onBtnClick = function onBtnClick(element) {
 "use strict";
 "use strict";
 
-/* When the user clicks on the button, 
-toggle between hiding and showing the dropdown content */
-function toggleMenu() {
-  document.getElementById("myDropdown").classList.toggle("show");
-}
-
-function selectItem() {
-  document.getElementById("myBtn").style.color = "black";
-  document.getElementById("myDropdown").classList.toggle("show");
-} // Close the dropdown menu if the user clicks outside of it
-
-
-window.onclick = function (event) {
-  if (!event.target.matches('.rh-filter')) {
-    var dropdowns = document.getElementsByClassName("rh-filter-menu");
-    var i;
-
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
-};
-"use strict";
-
 var acc = document.getElementsByClassName("rh-accordion");
 var i;
 
