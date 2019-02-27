7210bdbc06d23579da02facd51b23d55f3011a5a
Rebecca Ohlsson <rebecca@brightfactory.se>
2019-02-27 08:47:12 +0100

    #190 Filter (ej färdig)

--- public/js/components.js
+++ public/js/components.js
@@ -1,10 +1,38 @@
 "use strict";
-"use strict";
 
 //look ES6!!
 var onBtnClick = function onBtnClick(element) {
   element.innerHTML = 'You did it! 👍';
   console.log('tadaa');
+};
+"use strict";
+"use strict";
+
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function toggleMenu() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+function selectItem() {
+  document.getElementById("myBtn").style.color = "black";
+  document.getElementById("myDropdown").classList.toggle("show");
+} // Close the dropdown menu if the user clicks outside of it
+
+
+window.onclick = function (event) {
+  if (!event.target.matches('.rh-filter')) {
+    var dropdowns = document.getElementsByClassName("rh-filter-menu");
+    var i;
+
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
 };
 "use strict";
 
