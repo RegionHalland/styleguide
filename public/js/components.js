6887beecdc20b642876057afa3974f4246637a60
Viktor Sarge <viktor.sarge@regionhalland.se>
2019-03-01 09:46:20 +0100

    Bright factory changes

--- public/js/components.js
+++ public/js/components.js
@@ -1,92 +1,11 @@
 "use strict";
-<<<<<<< HEAD
-=======
 
-<<<<<<< HEAD
 //look ES6!!
 var onBtnClick = function onBtnClick(element) {
   element.innerHTML = 'You did it! 👍';
   console.log('tadaa');
 };
 "use strict";
-"use strict";
->>>>>>> 54576e3e19356fb5e2ae0860e712bebca924ae80
-
-/* When the user clicks on the button, 
-toggle between hiding and showing the dropdown content */
-function toggleMenu() {
-  document.getElementById("myDropdown").classList.toggle("show");
-}
-
-function selectItem() {
-  document.getElementById("myBtn").style.color = "black";
-  document.getElementById("myDropdown").classList.toggle("show");
-} // Close the dropdown menu if the user clicks outside of it
-
-
-window.onclick = function (event) {
-  if (!event.target.matches('.rh-filter')) {
-    var dropdowns = document.getElementsByClassName("rh-filter-menu");
-    var i;
-
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
-};
-"use strict";
-
-=======
->>>>>>> 9cc3691d1a61339f052b5d88cef1ad2e7c32f34b
-var acc = document.getElementsByClassName("rh-accordion");
-var i;
-
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function () {
-    this.classList.toggle("rh-accordion-active");
-    var panel = this.nextElementSibling;
-
-    if (panel.style.maxHeight) {
-      panel.style.maxHeight = null;
-    } else {
-      panel.style.maxHeight = panel.scrollHeight + "px";
-    }
-  });
-}
-"use strict";
-
-var videoPlayButton,
-    videoWrapper = document.getElementsByClassName('rh-video')[0],
-    video = document.getElementsByTagName('video')[0],
-    videoMethods = {
-  renderVideoPlayButton: function renderVideoPlayButton() {
-    if (videoWrapper.contains(video)) {
-      this.formatVideoPlayButton();
-      video.classList.add('has-media-controls-hidden');
-      videoPlayButton = document.getElementsByClassName('video-button')[0];
-      videoPlayButton.addEventListener('click', this.hideVideoPlayButton);
-    }
-  },
-  formatVideoPlayButton: function formatVideoPlayButton() {
-    videoWrapper.insertAdjacentHTML('beforeend', '\
-            <div class="video-button"><div class="video-button-content"> <i class="feather icon-play"></i> </div></div>\
-            ');
-  },
-  hideVideoPlayButton: function hideVideoPlayButton() {
-    video.play();
-    videoPlayButton.classList.add('is-hidden');
-    video.classList.remove('has-media-controls-hidden');
-    video.setAttribute('controls', 'controls');
-  }
-};
-videoMethods.renderVideoPlayButton();
-"use strict";
-<<<<<<< HEAD
-=======
 "use strict";
 
 function toggleMenu() {
@@ -129,15 +48,46 @@ window.onclick = function (event) {
   }
 };
 "use strict";
->>>>>>> 9cc3691d1a61339f052b5d88cef1ad2e7c32f34b
 
-//look ES6!!
-var onBtnClick = function onBtnClick(element) {
-  element.innerHTML = 'You did it! 👍';
-  console.log('tadaa');
-<<<<<<< HEAD
-};
+var acc = document.getElementsByClassName("rh-accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("rh-accordion-active");
+    var panel = this.nextElementSibling;
+
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
 "use strict";
-=======
+
+var videoPlayButton,
+    videoWrapper = document.getElementsByClassName('rh-video')[0],
+    video = document.getElementsByTagName('video')[0],
+    videoMethods = {
+  renderVideoPlayButton: function renderVideoPlayButton() {
+    if (videoWrapper.contains(video)) {
+      this.formatVideoPlayButton();
+      video.classList.add('has-media-controls-hidden');
+      videoPlayButton = document.getElementsByClassName('video-button')[0];
+      videoPlayButton.addEventListener('click', this.hideVideoPlayButton);
+    }
+  },
+  formatVideoPlayButton: function formatVideoPlayButton() {
+    videoWrapper.insertAdjacentHTML('beforeend', '\
+            <div class="video-button"><div class="video-button-content"> <i class="feather icon-play"></i> </div></div>\
+            ');
+  },
+  hideVideoPlayButton: function hideVideoPlayButton() {
+    video.play();
+    videoPlayButton.classList.add('is-hidden');
+    video.classList.remove('has-media-controls-hidden');
+    video.setAttribute('controls', 'controls');
+  }
 };
->>>>>>> 9cc3691d1a61339f052b5d88cef1ad2e7c32f34b
+videoMethods.renderVideoPlayButton();
