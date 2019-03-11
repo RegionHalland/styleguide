d0901f71a86586610fa51236295eebef610f48ef
molvig <jennie@brightfactory.se>
2019-03-11 12:31:03 +0100

    #187 Tabs

--- public/js/components.js
+++ public/js/components.js
@@ -1,11 +1,4 @@
 "use strict";
-
-//look ES6!!
-var onBtnClick = function onBtnClick(element) {
-  element.innerHTML = 'You did it! 👍';
-  console.log('tadaa');
-};
-"use strict";
 "use strict";
 
 function toggleMenu() {
@@ -46,6 +39,13 @@ window.onclick = function (event) {
       }
     }
   }
+};
+"use strict";
+
+//look ES6!!
+var onBtnClick = function onBtnClick(element) {
+  element.innerHTML = 'You did it! 👍';
+  console.log('tadaa');
 };
 "use strict";
 
@@ -107,74 +107,4 @@ for (i = 0; i < acc.length; i++) {
       panel.style.maxHeight = panel.scrollHeight + 100 + "px";
     }
   });
-}
-"use strict";
-
-window.onload = function () {
-  var table, rows, i, x, y;
-  table = document.getElementById("rh-table");
-  rows = table.rows;
-
-  for (var i = 1, row; row = rows[i]; i++) {
-    for (var j = 0, col; col = row.cells[j]; j++) {
-      x = rows[i].getElementsByTagName("TD")[j];
-      console.log(x.innerText);
-
-      if (x.innerText.length > 20) {
-        console.log(x.classList[0]);
-        x.classList.add('test');
-      }
-    }
-  }
-};
-
-function sortByName() {
-  var table,
-      rows,
-      switching,
-      i,
-      x,
-      y,
-      shouldSwitch,
-      dir,
-      switchcount = 0;
-  table = document.getElementById("rh-table");
-  switching = true;
-  dir = "asc";
-
-  while (switching) {
-    switching = false;
-    rows = table.getElementsByTagName("TR");
-
-    for (i = 1; i < rows.length - 1; i++) {
-      shouldSwitch = false;
-      x = rows[i].getElementsByTagName("TD")[1];
-      y = rows[i + 1].getElementsByTagName("TD")[1];
-      var xContent = isNaN(x.innerHTML) ? x.innerHTML.toLowerCase() === '-' ? 0 : x.innerHTML.toLowerCase() : parseFloat(x.innerHTML);
-      var yContent = isNaN(y.innerHTML) ? y.innerHTML.toLowerCase() === '-' ? 0 : y.innerHTML.toLowerCase() : parseFloat(y.innerHTML);
-
-      if (dir == "asc") {
-        if (xContent > yContent) {
-          shouldSwitch = true;
-          break;
-        }
-      } else if (dir == "desc") {
-        if (xContent < yContent) {
-          shouldSwitch = true;
-          break;
-        }
-      }
-    }
-
-    if (shouldSwitch) {
-      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
-      switching = true;
-      switchcount++;
-    } else {
-      if (switchcount == 0 && dir == "asc") {
-        dir = "desc";
-        switching = true;
-      }
-    }
-  }
 }
