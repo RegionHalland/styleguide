b8342f7d0ef9956f46af003c94eb712f583be26d
Viktor Sarge <viktor.sarge@regionhalland.se>
2019-03-13 12:09:39 +0100

    Regenerating files for 2.2.0

--- public/js/components.js
+++ public/js/components.js
@@ -1,4 +1,48 @@
 "use strict";
+
+var acc = document.getElementsByClassName("rh-accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("rh-accordion-active");
+    var panel = this.nextElementSibling;
+
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + 100 + "px";
+    }
+  });
+}
+"use strict";
+
+var videoPlayButton,
+    videoWrapper = document.getElementsByClassName('rh-video')[0],
+    video = document.getElementsByTagName('video')[0],
+    videoMethods = {
+  renderVideoPlayButton: function renderVideoPlayButton() {
+    if (videoWrapper !== undefined && videoWrapper.contains(video)) {
+      this.formatVideoPlayButton();
+      video.classList.add('has-media-controls-hidden');
+      videoPlayButton = document.getElementsByClassName('video-button')[0];
+      videoPlayButton.addEventListener('click', this.hideVideoPlayButton);
+    }
+  },
+  formatVideoPlayButton: function formatVideoPlayButton() {
+    videoWrapper.insertAdjacentHTML('beforeend', '\
+            <div class="video-button"><div class="video-button-content"> <i class="feather icon-play"></i> </div></div>\
+            ');
+  },
+  hideVideoPlayButton: function hideVideoPlayButton() {
+    video.play();
+    videoPlayButton.classList.add('is-hidden');
+    video.classList.remove('has-media-controls-hidden');
+    video.setAttribute('controls', 'controls');
+  }
+};
+videoMethods.renderVideoPlayButton();
+"use strict";
 "use strict";
 
 function toggleMenu() {
@@ -43,12 +87,12 @@ window.onclick = function (event) {
 "use strict";
 "use strict";
 
-var acc = document.getElementsByClassName("rh-accordion");
+var acc = document.getElementsByClassName("rh-search-accordion");
 var i;
 
 for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function () {
-    this.classList.toggle("rh-accordion-active");
+    this.classList.toggle("rh-search-active");
     var panel = this.nextElementSibling;
 
     if (panel.style.maxHeight) {
@@ -58,33 +102,6 @@ for (i = 0; i < acc.length; i++) {
     }
   });
 }
-"use strict";
-
-var videoPlayButton,
-    videoWrapper = document.getElementsByClassName('rh-video')[0],
-    video = document.getElementsByTagName('video')[0],
-    videoMethods = {
-  renderVideoPlayButton: function renderVideoPlayButton() {
-    if (videoWrapper !== undefined && videoWrapper.contains(video)) {
-      this.formatVideoPlayButton();
-      video.classList.add('has-media-controls-hidden');
-      videoPlayButton = document.getElementsByClassName('video-button')[0];
-      videoPlayButton.addEventListener('click', this.hideVideoPlayButton);
-    }
-  },
-  formatVideoPlayButton: function formatVideoPlayButton() {
-    videoWrapper.insertAdjacentHTML('beforeend', '\
-            <div class="video-button"><div class="video-button-content"> <i class="feather icon-play"></i> </div></div>\
-            ');
-  },
-  hideVideoPlayButton: function hideVideoPlayButton() {
-    video.play();
-    videoPlayButton.classList.add('is-hidden');
-    video.classList.remove('has-media-controls-hidden');
-    video.setAttribute('controls', 'controls');
-  }
-};
-videoMethods.renderVideoPlayButton();
 "use strict";
 
 window.onload = function () {
@@ -179,21 +196,4 @@ function setShadow() {
       cols[0].classList.add("rh-table-cell-shadow");
     }
   }
-}
-"use strict";
-
-var acc = document.getElementsByClassName("rh-search-accordion");
-var i;
-
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function () {
-    this.classList.toggle("rh-search-active");
-    var panel = this.nextElementSibling;
-
-    if (panel.style.maxHeight) {
-      panel.style.maxHeight = null;
-    } else {
-      panel.style.maxHeight = panel.scrollHeight + 100 + "px";
-    }
-  });
 }
