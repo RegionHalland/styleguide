67d68ad64fcdac19e96b4615de7d600d9ee94ba8
rhn014 <Roland.Hyden@RegionHalland.se>
2019-06-05 15:16:46 +0200

    Sites-hantering

--- build/js/components.js
+++ build/js/components.js
@@ -1,4 +1,47 @@
 "use strict";
+"use strict";
+
+function toggleMenu() {
+  document.getElementById("myDropdown").classList.toggle("rh-filter-show");
+  document.getElementById("dropdownBtn").classList.toggle("rh-filter-active");
+}
+
+function selectItem(sel) {
+  document.getElementById("dropdownBtn").classList.toggle("rh-filter-active");
+  document.getElementById("dropdownBtn").style.color = "black";
+  document.getElementById("myDropdown").classList.toggle("rh-filter-show");
+  var text = document.getElementById("dropdownBtn").firstChild;
+  text.data = sel.innerText;
+} // Close the dropdown menu if the user clicks outside of it
+
+
+window.onclick = function (event) {
+  if (!event.target.matches('.rh-filter')) {
+    var dropdowns = document.getElementsByClassName("rh-filter-menu");
+    var i;
+
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+
+      if (openDropdown.classList.contains('rh-filter-show')) {
+        openDropdown.classList.remove('rh-filter-show');
+      }
+    }
+
+    var btn = document.getElementsByClassName("rh-filter");
+    var i;
+
+    for (i = 0; i < btn.length; i++) {
+      var activeBtn = btn[i];
+
+      if (activeBtn.classList.contains('rh-filter-active')) {
+        activeBtn.classList.remove('rh-filter-active');
+      }
+    }
+  }
+};
+"use strict";
+"use strict";
 
 var acc = document.getElementsByClassName("rh-search-accordion");
 var i;
@@ -110,49 +153,6 @@ function setShadow() {
     }
   }
 }
-"use strict";
-"use strict";
-
-function toggleMenu() {
-  document.getElementById("myDropdown").classList.toggle("rh-filter-show");
-  document.getElementById("dropdownBtn").classList.toggle("rh-filter-active");
-}
-
-function selectItem(sel) {
-  document.getElementById("dropdownBtn").classList.toggle("rh-filter-active");
-  document.getElementById("dropdownBtn").style.color = "black";
-  document.getElementById("myDropdown").classList.toggle("rh-filter-show");
-  var text = document.getElementById("dropdownBtn").firstChild;
-  text.data = sel.innerText;
-} // Close the dropdown menu if the user clicks outside of it
-
-
-window.onclick = function (event) {
-  if (!event.target.matches('.rh-filter')) {
-    var dropdowns = document.getElementsByClassName("rh-filter-menu");
-    var i;
-
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-
-      if (openDropdown.classList.contains('rh-filter-show')) {
-        openDropdown.classList.remove('rh-filter-show');
-      }
-    }
-
-    var btn = document.getElementsByClassName("rh-filter");
-    var i;
-
-    for (i = 0; i < btn.length; i++) {
-      var activeBtn = btn[i];
-
-      if (activeBtn.classList.contains('rh-filter-active')) {
-        activeBtn.classList.remove('rh-filter-active');
-      }
-    }
-  }
-};
-"use strict";
 "use strict";
 
 var acc = document.getElementsByClassName("rh-accordion");
--- gulpfile.js
+++ gulpfile.js
@@ -9,6 +9,7 @@ const image = require('gulp-image');
 const project = require('./package.json');
 const rename = require("gulp-rename");
 const babel = require('gulp-babel');
+const jsImport = require('gulp-js-import');
 
 const mandelbrot = require('@frctl/mandelbrot');
 const fractal = require('@frctl/fractal').create();
@@ -29,7 +30,6 @@ const rh_theme = mandelbrot({
 });
 
 rh_theme.addLoadPath(__dirname + '/assets/theme/views/');
-
 
 fractal.set('project.title', 'Stilguide - Region Halland');
 
@@ -67,7 +67,6 @@ fractal.components.set('statuses', {
     },
 });
 
-
 function start(cb) {
 
     gulp.watch('components/**/*', parallel(scss, images, js));
@@ -83,7 +82,6 @@ function start(cb) {
 
     cb();
 }
-
 
 function theme(cb) {
 
@@ -118,7 +116,6 @@ function theme(cb) {
 
 }
 
-
 function release(cb) {
 
     images(cb);
@@ -143,7 +140,7 @@ function release(cb) {
 }
 
 function scss(cb) {
- 	gulp.src('components/**/*.scss')
+ 	gulp.src('components/scss/main.scss')
 		.pipe(sourcemaps.init())
   		.pipe(
 			sass.sync({
@@ -158,7 +155,6 @@ function scss(cb) {
     logger.success('Kompilerade komponent SCSS');
 	cb();
 }
-
 
 function images(cb) {
  	gulp.src('components/**/*.{svg,png,gif,jpg}')
@@ -191,8 +187,6 @@ function js(cb) {
 	cb();
 }
 
-
-
 function build(cb) {
     const builder = fractal.web.builder();
     builder.on('progress', (completed, total) => logger.update(`Exported ${completed} of ${total} items`, 'info'));
@@ -203,8 +197,8 @@ function build(cb) {
     cb();
 }
 
-function tmp(cb) {
-    gulp.src('components/scss/tmp.scss')
+function tmp_build_scss(cb) {
+    gulp.src('sites/tmp/tmp.scss')
         .pipe(sourcemaps.init())
         .pipe(
             sass.sync({
@@ -220,7 +214,322 @@ function tmp(cb) {
     cb();
 }
 
+// *****************
+// *** Driftinfo ***
+// *****************
+function driftinfo_build_js(cb) {
+    var src = [
+       'components/Organism/rh-table/rh-table.js', 
+       'components/Organism/rh-search/rh-search.js'
+    ];
+    gulp.src(src)
+        .pipe(babel({
+            presets: ['@babel/env']
+        }))
+        .pipe(rename(function (path) {
+            console.log(path);
+            var p = path.dirname.split('/');
+            path.dirname = 'components/' + p[p.length -1];
+        }))
+        .pipe(concat('driftinfo.js'))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest(fractal.web.get('static.path') + '/js/'));
+
+    logger.success('Kompilerade komponent JS');
+    cb();
+}
+function driftinfo_build_scss(cb) {
+    gulp.src('sites/driftinfo/driftinfo.scss')
+        .pipe(sourcemaps.init())
+        .pipe(
+            sass.sync({
+                outputStyle: 'compressed',
+            })
+            .on('error', sass.logError)
+        )
+        .pipe(concat('driftinfo.css'))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest(fractal.web.get('static.path') + '/css/'));
+
+    logger.success('Kompilerade komponent SCSS');
+    cb();
+}
+function driftinfo_release(cb) {
+
+    driftinfo_build_scss(cb);
+    driftinfo_build_js(cb);
+    
+    gulp.src('./public/css/driftinfo.css')
+        .pipe(rename("driftinfo."+project.version_driftinfo+".css"))
+        .pipe(gulp.dest('./releases/driftinfo'))
+
+    gulp.src('./public/js/driftinfo.js')
+        .pipe(rename("driftinfo."+project.version_driftinfo+".js"))
+        .pipe(gulp.dest('./releases/driftinfo'))
+
+    logger.success('Version ' + project.version_driftinfo + ' vänter på dig i /releases/! 👏👏👏')
+
+    cb();
+}
+
+// **********************
+// *** Region Halland ***
+// **********************
+function regionhalland_build_js(cb) {
+    var src = [
+       'components/Organism/rh-table/rh-table.js', 
+       'components/Organism/rh-search/rh-search.js'
+    ];
+    gulp.src(src)
+        .pipe(babel({
+            presets: ['@babel/env']
+        }))
+        .pipe(rename(function (path) {
+            console.log(path);
+            var p = path.dirname.split('/');
+            path.dirname = 'components/' + p[p.length -1];
+        }))
+        .pipe(concat('regionhalland.js'))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest(fractal.web.get('static.path') + '/js/'));
+
+    logger.success('Kompilerade komponent JS');
+    cb();
+}
+function regionhalland_build_scss(cb) {
+    gulp.src('sites/regionhalland/regionhalland.scss')
+        .pipe(sourcemaps.init())
+        .pipe(
+            sass.sync({
+                outputStyle: 'compressed',
+            })
+            .on('error', sass.logError)
+        )
+        .pipe(concat('regionhalland.css'))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest(fractal.web.get('static.path') + '/css/'));
+
+    logger.success('Kompilerade komponent SCSS');
+    cb();
+}
+function regionhalland_release(cb) {
+
+    regionhalland_build_scss(cb);
+    regionhalland_build_js(cb);
+    
+    gulp.src('./public/css/regionhalland.css')
+        .pipe(rename("regionhalland."+project.version_regionhalland+".css"))
+        .pipe(gulp.dest('./releases/regionhalland'))
+
+    gulp.src('./public/js/regionhalland.js')
+        .pipe(rename("regionhalland."+project.version_regionhalland+".js"))
+        .pipe(gulp.dest('./releases/regionhalland'))
+
+    logger.success('Version ' + project.version_regionhalland + ' vänter på dig i /releases/! 👏👏👏')
+
+    cb();
+}
+
+// **********************
+// *** Musikhallandia ***
+// **********************
+function musikhallandia_build_js(cb) {
+    var src = [
+       'components/Organism/rh-table/rh-table.js', 
+       'components/Organism/rh-search/rh-search.js'
+    ];
+    gulp.src(src)
+        .pipe(babel({
+            presets: ['@babel/env']
+        }))
+        .pipe(rename(function (path) {
+            console.log(path);
+            var p = path.dirname.split('/');
+            path.dirname = 'components/' + p[p.length -1];
+        }))
+        .pipe(concat('musikhallandia.js'))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest(fractal.web.get('static.path') + '/js/'));
+
+    logger.success('Kompilerade komponent JS');
+    cb();
+}
+function musikhallandia_build_scss(cb) {
+    gulp.src('sites/musikhallandia/musikhallandia.scss')
+        .pipe(sourcemaps.init())
+        .pipe(
+            sass.sync({
+                outputStyle: 'compressed',
+            })
+            .on('error', sass.logError)
+        )
+        .pipe(concat('musikhallandia.css'))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest(fractal.web.get('static.path') + '/css/'));
+
+    logger.success('Kompilerade komponent SCSS');
+    cb();
+}
+function musikhallandia_release(cb) {
+
+    musikhallandia_build_scss(cb);
+    musikhallandia_build_js(cb);
+    
+    gulp.src('./public/css/musikhallandia.css')
+        .pipe(rename("musikhallandia."+project.version_musikhallandia+".css"))
+        .pipe(gulp.dest('./releases/musikhallandia'))
+
+    gulp.src('./public/js/musikhallandia.js')
+        .pipe(rename("musikhallandia."+project.version_musikhallandia+".js"))
+        .pipe(gulp.dest('./releases/musikhallandia'))
+
+    logger.success('Version ' + project.version_musikhallandia + ' vänter på dig i /releases/! 👏👏👏')
+
+    cb();
+}
+
+// ***********************
+// *** Vårdgivarwebben ***
+// ***********************
+function vardgivare_build_js(cb) {
+    var src = [
+       'components/Organism/rh-table/rh-table.js', 
+       'components/Organism/rh-search/rh-search.js'
+    ];
+    gulp.src(src)
+        .pipe(babel({
+            presets: ['@babel/env']
+        }))
+        .pipe(rename(function (path) {
+            console.log(path);
+            var p = path.dirname.split('/');
+            path.dirname = 'components/' + p[p.length -1];
+        }))
+        .pipe(concat('vardgivare.js'))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest(fractal.web.get('static.path') + '/js/'));
+
+    logger.success('Kompilerade komponent JS');
+    cb();
+}
+function vardgivare_build_scss(cb) {
+    gulp.src('sites/vardgivare/vardgivare.scss')
+        .pipe(sourcemaps.init())
+        .pipe(
+            sass.sync({
+                outputStyle: 'compressed',
+            })
+            .on('error', sass.logError)
+        )
+        .pipe(concat('vardgivare.css'))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest(fractal.web.get('static.path') + '/css/'));
+
+    logger.success('Kompilerade komponent SCSS');
+    cb();
+}
+function vardgivare_release(cb) {
+
+    vardgivare_build_scss(cb);
+    vardgivare_build_js(cb);
+    
+    gulp.src('./public/css/vardgivare.css')
+        .pipe(rename("vardgivare."+project.version_vardgivare+".css"))
+        .pipe(gulp.dest('./releases/vardgivare'))
+
+    gulp.src('./public/js/vardgivare.js')
+        .pipe(rename("vardgivare."+project.version_vardgivare+".js"))
+        .pipe(gulp.dest('./releases/vardgivare'))
+
+    logger.success('Version ' + project.version_vardgivare + ' vänter på dig i /releases/! 👏👏👏')
+
+    cb();
+}
+
+// ******************
+// *** Vuxhalland ***
+// ******************
+function vuxhalland_build_js(cb) {
+    var src = [
+       'components/Organism/rh-table/rh-table.js', 
+       'components/Organism/rh-search/rh-search.js'
+    ];
+    gulp.src(src)
+        .pipe(babel({
+            presets: ['@babel/env']
+        }))
+        .pipe(rename(function (path) {
+            console.log(path);
+            var p = path.dirname.split('/');
+            path.dirname = 'components/' + p[p.length -1];
+        }))
+        .pipe(concat('vuxhalland.js'))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest(fractal.web.get('static.path') + '/js/'));
+
+    logger.success('Kompilerade komponent JS');
+    cb();
+}
+function vuxhalland_build_scss(cb) {
+    gulp.src('sites/vuxhalland/vuxhalland.scss')
+        .pipe(sourcemaps.init())
+        .pipe(
+            sass.sync({
+                outputStyle: 'compressed',
+            })
+            .on('error', sass.logError)
+        )
+        .pipe(concat('vuxhalland.css'))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest(fractal.web.get('static.path') + '/css/'));
+
+    logger.success('Kompilerade komponent SCSS');
+    cb();
+}
+function vuxhalland_release(cb) {
+
+    vuxhalland_build_scss(cb);
+    vuxhalland_build_js(cb);
+    
+    gulp.src('./public/css/vuxhalland.css')
+        .pipe(rename("vuxhalland."+project.version_vuxhalland+".css"))
+        .pipe(gulp.dest('./releases/vuxhalland'))
+
+    gulp.src('./public/js/vuxhalland.js')
+        .pipe(rename("vuxhalland."+project.version_vuxhalland+".js"))
+        .pipe(gulp.dest('./releases/vuxhalland'))
+
+    logger.success('Version ' + project.version_vuxhalland + ' vänter på dig i /releases/! 👏👏👏')
+
+    cb();
+}
+
+// Tmp
+exports.tmp_build = series(tmp_build_scss);
+
+// Driftinfo
+exports.driftinfo_build = series(driftinfo_build_scss, driftinfo_build_js);
+exports.driftinfo_release = series(driftinfo_release);
+
+// Musikhallandia
+exports.musikhallandia_build = series(musikhallandia_build_scss, musikhallandia_build_js);
+exports.musikhallandia_release = series(musikhallandia_release);
+
+// Region Halland
+exports.regionhalland_build = series(regionhalland_build_scss, regionhalland_build_js);
+exports.regionhalland_release = series(regionhalland_release);
+
+// Vardgivarwebben
+exports.vardgivare_build = series(vardgivare_build_scss, vardgivare_build_js);
+exports.vardgivare_release = series(vardgivare_release);
+
+// Vuxhalland
+exports.vuxhalland_build = series(vuxhalland_build_scss, vuxhalland_build_js);
+exports.vuxhalland_release = series(vuxhalland_release);
+
+// Main
 exports.version = series(build, release);
 exports.build = series(start, build);
-exports.tmp = series(tmp);
 exports.default = start;
+
+
