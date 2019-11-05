570f76d08a052c02e8c3c06b5ca383f9195c6c26
Viktor Sarge <viktor.sarge@regionhalland.se>
2019-11-05 11:15:54 +0200

    Webbplatsen byggd

--- build/js/components.js
+++ build/js/components.js
@@ -529,6 +529,20 @@ $(document).ready(function () {
 });
 "use strict";
 
+$(document).ready(function () {
+  // This code fixs :focus-within behavior on IE11 and older browsers
+  var $navigationBlockItems = $(".rh-navigation-block");
+  $navigationBlockItems.focusin(function (e) {
+    e.stopPropagation();
+    $(this).addClass("rh-navigation-block--focus");
+  });
+  $navigationBlockItems.focusout(function (e) {
+    e.stopPropagation();
+    $(this).removeClass("rh-navigation-block--focus");
+  });
+});
+"use strict";
+
 var videoPlayButton,
     videoWrapper = document.getElementsByClassName('rh-video')[0],
     video = document.getElementsByTagName('video')[0],
