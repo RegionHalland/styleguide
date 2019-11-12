9cee1ccdaba8dde695cf3ab65c834b80d5d3bb78
Viktor Sarge <viktor.sarge@regionhalland.se>
2019-11-12 19:23:50 +0200

    Webbplatsen byggd

--- build/js/components.js
+++ build/js/components.js
@@ -155,20 +155,6 @@ function setShadow() {
 "use strict";
 "use strict";
 
-$(document).ready(function () {
-  // This code fixs :focus-within behavior on IE11 and older browsers
-  var $blockBoxItems = $(".rh-block-box");
-  $blockBoxItems.focusin(function (e) {
-    e.stopPropagation();
-    $(this).addClass("rh-block--focus");
-  });
-  $blockBoxItems.focusout(function (e) {
-    e.stopPropagation();
-    $(this).removeClass("rh-block--focus");
-  });
-});
-"use strict";
-
 // Needed function:
 // throttle() - /public/library.js
 $(document).ready(function () {
@@ -191,6 +177,20 @@ $(document).ready(function () {
     $('body,html').animate({
       scrollTop: 0
     }, 800);
+  });
+});
+"use strict";
+
+$(document).ready(function () {
+  // This code fixs :focus-within behavior on IE11 and older browsers
+  var $blockBoxItems = $(".rh-block-box");
+  $blockBoxItems.focusin(function (e) {
+    e.stopPropagation();
+    $(this).addClass("rh-block--focus");
+  });
+  $blockBoxItems.focusout(function (e) {
+    e.stopPropagation();
+    $(this).removeClass("rh-block--focus");
   });
 });
 "use strict";
