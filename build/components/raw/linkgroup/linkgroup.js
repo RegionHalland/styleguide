9bdf8210ff03c84831a0d599dfeda0c0c7159ffe
Viktor Sarge <viktor.sarge@regionhalland.se>
2020-01-08 14:29:23 +0200

    Byggt sajten efter ändringar i 12.0.0

--- build/components/raw/linkgroup/linkgroup.js
+++ build/components/raw/linkgroup/linkgroup.js
@@ -1,14 +1,5 @@
-var acc = document.getElementsByClassName("rh-accordion");
-var i;
-
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    this.classList.toggle("rh-accordion-active");
-    var panel = this.nextElementSibling;
-    if (panel.style.maxHeight){
-      panel.style.maxHeight = null;
-    } else {
-      panel.style.maxHeight = panel.scrollHeight + 100 + "px";
-    } 
-  });
-}
+$(".rh-linkgroup__titlebar").click(function(){
+    $(this).parent().next().toggle();
+    $(this).toggleClass("rh-linkgroup__titlebar--active");
+    $(this).attr('aria-expanded',$(this).attr('aria-expanded')==='true'?'false':'true' );
+});
