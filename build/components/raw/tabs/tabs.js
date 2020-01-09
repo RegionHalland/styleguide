9ab7a8201538f913ddd3a1ad6bd07aefeb012b83
Viktor Sarge <viktor.sarge@regionhalland.se>
2020-01-09 15:25:37 +0200

    Byggt sajten för 13.0.0

---
+++ build/components/raw/tabs/tabs.js
@@ -0,0 +1,19 @@
+$(".rh-tabs__item").keydown(function() {
+    console.log(event.which);
+    switch (event.which) {
+        case 39:
+            if($(this).next().length == 0) {
+                $(this).siblings().first().focus();
+            } else {
+                $(this).next().focus();
+            }
+            break;
+        case 37:
+            if($(this).prev().length == 0) {
+                $(this).siblings().last().focus();
+            } else {
+                $(this).prev().focus();
+            }
+            break;
+    }
+});
