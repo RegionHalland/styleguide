8633196bd3de9404b28800f2dba390e705390588
Viktor Sarge <viktor.sarge@regionhalland.se>
2020-01-17 12:34:23 +0200

    Byggt sajten för dokumentation av nya statusar

--- build/components/raw/video/video.js
+++ build/components/raw/video/video.js
@@ -1,29 +1,28 @@
 var videoPlayButton,
-	videoWrapper = document.getElementsByClassName('rh-video')[0],
+    videoWrapper = document.getElementsByClassName('rh-video')[0],
     video = document.getElementsByTagName('video')[0],
     videoMethods = {
         renderVideoPlayButton: function() {
             if (videoWrapper !== undefined && videoWrapper.contains(video)) {
-				this.formatVideoPlayButton()
-                video.classList.add('has-media-controls-hidden')
-                videoPlayButton = document.getElementsByClassName('video-button')[0]
-                videoPlayButton.addEventListener('click', this.hideVideoPlayButton)
+                this.formatVideoPlayButton();
+                video.classList.add('has-media-controls-hidden');
+                videoPlayButton = document.getElementsByClassName('video-button')[0];
+                videoPlayButton.addEventListener('click', this.hideVideoPlayButton);
             }
         },
 
         formatVideoPlayButton: function() {
             videoWrapper.insertAdjacentHTML('beforeend', '\
             <div class="video-button"><div class="video-button-content"> <i class="feather icon-play"></i> </div></div>\
-            ')
+            ');
         },
 
         hideVideoPlayButton: function() {
-            video.play()
-            videoPlayButton.classList.add('is-hidden')
-            video.classList.remove('has-media-controls-hidden')
-            video.setAttribute('controls', 'controls')
+            video.play();
+            videoPlayButton.classList.add('is-hidden');
+            video.classList.remove('has-media-controls-hidden');
+            video.setAttribute('controls', 'controls');
         }
-	}
+    };
 
-videoMethods.renderVideoPlayButton()
-
+videoMethods.renderVideoPlayButton();
