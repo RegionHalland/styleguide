38e626fbf0e0fef698b3b65e63115ae5fbf15a7c
Viktor Sarge <viktor.sarge@regionhalland.se>
2020-01-22 16:25:58 +0200

    molecules/navigation-bar fått beteende för piltangenterna

--- public/js/components.js
+++ public/js/components.js
@@ -1,4 +1,116 @@
 "use strict";
+
+var acc = document.getElementsByClassName("rh-search-accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("rh-search-active");
+    var panel = this.nextElementSibling;
+
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + 100 + "px";
+    }
+  });
+}
+"use strict";
+
+window.onload = function () {
+  var table, rows, i, x, y;
+  table = document.getElementById("rh-table");
+
+  if (table) {
+    rows = table.rows;
+
+    for (var i = 1, row; row = rows[i]; i++) {
+      for (var j = 0, col; col = row.cells[j]; j++) {
+        x = rows[i].getElementsByTagName("TD")[j];
+
+        if (x.innerText.length > 20) {
+          x.classList.add('show-icon');
+        }
+      }
+    }
+  }
+};
+
+function sortByName() {
+  var table,
+      rows,
+      switching,
+      i,
+      x,
+      y,
+      shouldSwitch,
+      dir,
+      switchcount = 0;
+  table = document.getElementById("rh-table");
+  switching = true;
+  dir = "asc";
+
+  while (switching) {
+    switching = false;
+    rows = table.getElementsByTagName("TR");
+
+    for (i = 1; i < rows.length - 1; i++) {
+      shouldSwitch = false;
+      x = rows[i].getElementsByTagName("TD")[1];
+      y = rows[i + 1].getElementsByTagName("TD")[1];
+      var xContent = isNaN(x.innerHTML) ? x.innerHTML.toLowerCase() === '-' ? 0 : x.innerHTML.toLowerCase() : parseFloat(x.innerHTML);
+      var yContent = isNaN(y.innerHTML) ? y.innerHTML.toLowerCase() === '-' ? 0 : y.innerHTML.toLowerCase() : parseFloat(y.innerHTML);
+
+      if (dir == "asc") {
+        if (xContent > yContent) {
+          shouldSwitch = true;
+          break;
+        }
+      } else if (dir == "desc") {
+        if (xContent < yContent) {
+          shouldSwitch = true;
+          break;
+        }
+      }
+    }
+
+    if (shouldSwitch) {
+      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+      switching = true;
+      switchcount++;
+    } else {
+      if (switchcount == 0 && dir == "asc") {
+        dir = "desc";
+        switching = true;
+      }
+    }
+  }
+}
+
+function expand() {
+  var rows = document.getElementById('rh-table').rows;
+
+  for (var row = 0; row < rows.length; row++) {
+    var cols = rows[row].cells;
+
+    if (2 >= 0 && 2 < cols.length) {
+      cols[2].classList.toggle("selected");
+    }
+  }
+}
+
+function setShadow() {
+  var rows = document.getElementById('rh-table').rows;
+
+  for (var row = 0; row < rows.length; row++) {
+    var cols = rows[row].cells;
+
+    if (0 >= 0 && 0 < cols.length) {
+      cols[0].classList.add("rh-table-cell--shadow");
+    }
+  }
+}
+"use strict";
 "use strict";
 
 function toggleMenu() {
@@ -465,6 +577,29 @@ $(document).ready(function () {
 });
 "use strict";
 
+$(".rh-navigation-bar__link").keydown(function () {
+  switch (event.which) {
+    case 39:
+      if ($(this).parent().next().length === 0) {
+        $(this).parent().siblings().first().children().first().focus();
+      } else {
+        $(this).parent().next().children().first().focus();
+      }
+
+      break;
+
+    case 37:
+      if ($(this).parent().prev().length === 0) {
+        $(this).parent().siblings().last().children().first().focus();
+      } else {
+        $(this).parent().prev().children().first().focus();
+      }
+
+      break;
+  }
+});
+"use strict";
+
 $(".rh-tabs__item").keydown(function () {
   console.log(event.which);
 
@@ -515,115 +650,3 @@ var videoPlayButton,
   }
 };
 videoMethods.renderVideoPlayButton();
-"use strict";
-
-var acc = document.getElementsByClassName("rh-search-accordion");
-var i;
-
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function () {
-    this.classList.toggle("rh-search-active");
-    var panel = this.nextElementSibling;
-
-    if (panel.style.maxHeight) {
-      panel.style.maxHeight = null;
-    } else {
-      panel.style.maxHeight = panel.scrollHeight + 100 + "px";
-    }
-  });
-}
-"use strict";
-
-window.onload = function () {
-  var table, rows, i, x, y;
-  table = document.getElementById("rh-table");
-
-  if (table) {
-    rows = table.rows;
-
-    for (var i = 1, row; row = rows[i]; i++) {
-      for (var j = 0, col; col = row.cells[j]; j++) {
-        x = rows[i].getElementsByTagName("TD")[j];
-
-        if (x.innerText.length > 20) {
-          x.classList.add('show-icon');
-        }
-      }
-    }
-  }
-};
-
-function sortByName() {
-  var table,
-      rows,
-      switching,
-      i,
-      x,
-      y,
-      shouldSwitch,
-      dir,
-      switchcount = 0;
-  table = document.getElementById("rh-table");
-  switching = true;
-  dir = "asc";
-
-  while (switching) {
-    switching = false;
-    rows = table.getElementsByTagName("TR");
-
-    for (i = 1; i < rows.length - 1; i++) {
-      shouldSwitch = false;
-      x = rows[i].getElementsByTagName("TD")[1];
-      y = rows[i + 1].getElementsByTagName("TD")[1];
-      var xContent = isNaN(x.innerHTML) ? x.innerHTML.toLowerCase() === '-' ? 0 : x.innerHTML.toLowerCase() : parseFloat(x.innerHTML);
-      var yContent = isNaN(y.innerHTML) ? y.innerHTML.toLowerCase() === '-' ? 0 : y.innerHTML.toLowerCase() : parseFloat(y.innerHTML);
-
-      if (dir == "asc") {
-        if (xContent > yContent) {
-          shouldSwitch = true;
-          break;
-        }
-      } else if (dir == "desc") {
-        if (xContent < yContent) {
-          shouldSwitch = true;
-          break;
-        }
-      }
-    }
-
-    if (shouldSwitch) {
-      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
-      switching = true;
-      switchcount++;
-    } else {
-      if (switchcount == 0 && dir == "asc") {
-        dir = "desc";
-        switching = true;
-      }
-    }
-  }
-}
-
-function expand() {
-  var rows = document.getElementById('rh-table').rows;
-
-  for (var row = 0; row < rows.length; row++) {
-    var cols = rows[row].cells;
-
-    if (2 >= 0 && 2 < cols.length) {
-      cols[2].classList.toggle("selected");
-    }
-  }
-}
-
-function setShadow() {
-  var rows = document.getElementById('rh-table').rows;
-
-  for (var row = 0; row < rows.length; row++) {
-    var cols = rows[row].cells;
-
-    if (0 >= 0 && 0 < cols.length) {
-      cols[0].classList.add("rh-table-cell--shadow");
-    }
-  }
-}
